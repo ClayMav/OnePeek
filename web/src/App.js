6643176de9e266d6c605ee5814b114dfdb9d9561
Clay McGinnis <mcginnis@claymav.com>
2019-11-02 17:54:10 -0500

    Added base routes

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Login } from "./screens/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/passage">
+          <h1>Bye</h1>
+        </Route>
+        <Route path="/profile">
+          <h1>See ya</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

d0cd786466cce6aabb5217cba6344f93bca220bd
LanceWengLin <lancewenglin@gmail.com>
2019-11-03 03:05:27 -0600

    Setting up users table for prototype. Also remove the auth function now.

--- api/userinfo/models.py
+++ api/userinfo/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class UserInfo(models.Model):
-    p_read = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, related_name="p_read", on_delete=models.CASCADE, null=True)
-    #created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=255, null = True)
+    last_name = models.CharField(max_length=255, null = True)
+    p_read = models.CharField(max_length=255, null = True)

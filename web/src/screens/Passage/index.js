87ddf24167c3bb12d6f0579fe41b7963db8b99dd
Pierre Shum <pierrecshum@gmail.com>
2019-11-03 02:42:05 -0600

    Changes added to index.js

--- web/src/screens/Passage/index.js
+++ web/src/screens/Passage/index.js
@@ -56,13 +56,22 @@ const BookInfoBigger = styled.h4`
   font-size: 14px;
   color: #AFAFAF;
   font-weight: normal;
+  margin-bottom: 0;
+  margin-top: 0;
 `;
 
 const BuyNow = styled.a`
   font-size: 14px;
   color: #FFF2AF;
   font-weight: normal;
+
 `;
+
+const GroupDown = styled.div`
+  margin-top: 15px;
+
+`;
+
 
 const passage = {
   genres: ["IDK"],
@@ -104,10 +113,10 @@ const BookDetails = () => {
         <div>
           <BookInfoBigger>October 19th, 1953</BookInfoBigger>
           <BookInfoBigger>United States</BookInfoBigger>
-          <BookInfoBigger>The Sieve and the Sand, p. 132</BookInfoBigger>
-          <h4>
+          <GroupDown>
+            <BookInfoBigger>The Sieve and the Sand, p. 132</BookInfoBigger>
             <BuyNow>Buy on Amazon</BuyNow>
-          </h4>
+          </GroupDown>
         </div>
       </div>
     </div>
@@ -146,6 +155,7 @@ const Passage = () => {
         <Rating>Did you like this passage?</Rating>
         <a>
           <img />
+
         </a>
         <a>
           <img />
@@ -155,7 +165,9 @@ const Passage = () => {
       <hr />
       <BookDetails />
       <hr />
+
       <Trivia question={passage.questions[0]} />
+
     </Wrap>
   );
 };

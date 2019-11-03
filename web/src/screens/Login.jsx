caf8a545d943ee7581e347d2e46e6e229d843979
Clay McGinnis <mcginnis@claymav.com>
2019-11-02 21:47:39 -0500

    Login is chilling

--- web/src/screens/Login.jsx
+++ web/src/screens/Login.jsx
@@ -1,12 +1,38 @@
-import React, { setGlobal } from "reactn";
+import React, { setGlobal, useState } from "reactn";
 import styled from "styled-components";
 
 const Title = styled.h1`
+  color: white;
   margin-top: 0;
+`;
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
 `;
 
 const Login = () => {
-  return <Title>Login</Title>;
+  const [input, setInput] = useState("");
+
+  const onClick = () => {
+    console.log(`login ${input}`);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrap>
+      <Title>Login</Title>
+      <form onSubmit={handleSubmit}>
+        <input value={input} onChange={e => setInput(e.target.value)} />
+        <button onClick={onClick}>Login</button>
+      </form>
+    </Wrap>
+  );
 };
 
 export { Login };

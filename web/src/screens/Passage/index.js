165153051147597610613915a5b6be6934a7dae8
Pierre Shum <pierrecshum@gmail.com>
2019-11-03 01:50:37 -0600

    updated css

--- web/src/screens/Passage/index.js
+++ web/src/screens/Passage/index.js
@@ -7,6 +7,63 @@ const Wrap = styled.div`
   color: white;
 `;
 
+const Hello = styled.p`
+  font-size: 14px;
+`;
+
+const UsernameColors = styled.span`
+  color: #A9FFC1;
+`;
+
+const Personalized = styled.span`
+  color: #A5D9FE;
+`;
+
+const Paragraph = styled.p`
+  font-size: 16px;
+`;
+
+const BookTitle = styled.h1`
+  font-size: 28px;
+  margin-bottom: 0;
+`;
+
+const BookInfo = styled.h4`
+  font-size: 12px;
+  color: #AFAFAF;
+  margin-top: 0;
+`;
+
+const Rating = styled.h3
+`
+  font-size: 18px;
+`;
+
+const BookTitleBigger = styled.h1
+`
+font-size: 30px;
+margin-bottom: 0;
+`;
+
+const BookAuthor = styled.div
+`
+font-size: 14px;
+color: #AFAFAF;
+margin-top: 0;
+`;
+
+const BookInfoBigger = styled.h4`
+  font-size: 14px;
+  color: #AFAFAF;
+  font-weight: normal;
+`;
+
+const BuyNow = styled.a`
+  font-size: 14px;
+  color: #FFF2AF;
+  font-weight: normal;
+`;
+
 const passage = {
   genres: ["IDK"],
   authors: ["Ray Bradbury"],
@@ -39,17 +96,17 @@ const BookDetails = () => {
         <a>
           <img />
         </a>
-        <h1>Fahrenheit 451</h1>
+        <BookTitleBigger>Fahrenheit 451</BookTitleBigger>
       </div>
-      <div>Ray Bradbury</div>
+      <BookAuthor>Ray Bradbury</BookAuthor>
       <div>
         <img />
         <div>
-          <h4>October 19th, 1953</h4>
-          <h4>United States</h4>
-          <h4>The Sieve and the Sand, p. 132</h4>
+          <BookInfoBigger>October 19th, 1953</BookInfoBigger>
+          <BookInfoBigger>United States</BookInfoBigger>
+          <BookInfoBigger>The Sieve and the Sand, p. 132</BookInfoBigger>
           <h4>
-            <a>Buy on Amazon</a>
+            <BuyNow>Buy on Amazon</BuyNow>
           </h4>
         </div>
       </div>
@@ -57,18 +114,20 @@ const BookDetails = () => {
   );
 };
 
+
+
 const Passage = () => {
   return (
     <Wrap>
       <div>
         <img />
-        <p>
-          Hey <span>Clay</span>, this one's for <span>you</span>
-        </p>
+        <Hello>
+          Hey <UsernameColors>Clay</UsernameColors>, this one's for <Personalized>you</Personalized>
+        </Hello>
       </div>
-      <h1>Fahrenheit 451</h1>
-      <h4>1953 - Ray Bradbury - United States</h4>
-      <p>
+      <BookTitle>Fahrenheit 451</BookTitle>
+      <BookInfo>1953 - Ray Bradbury - United States</BookInfo>
+      <Paragraph>
         Yellowbanded perch, slender mola flabby whalefish cuchia bobtail snipe
         eel blue shark jack, armored searobin nurse shark huchen. Sandperch
         pelican eel, "dragonet scythe butterfish bocaccio grunter Blenny giant
@@ -81,10 +140,10 @@ const Passage = () => {
         eater bleak longnose sucker clingfish soapfish Ganges shark." Pikeblenny
         pumpkinseed tang: Bengal danio bluefin tuna pelagic cod monkfish gray
         mullet pilchard catfish sand tiger sawfish croaker; boafish.
-      </p>
+      </Paragraph>
 
       <div>
-        <h3>Did you like this passage?</h3>
+        <Rating>Did you like this passage?</Rating>
         <a>
           <img />
         </a>

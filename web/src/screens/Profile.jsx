5af6ff5330a89292e97b251b7d9e18d2f30ce240
Tom Dong <translucentleaf@protonmail.com>
2019-11-03 08:18:29 -0600

    Finished profile screen

--- web/src/screens/Profile.jsx
+++ web/src/screens/Profile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { BookmarkBorder } from "@material-ui/icons"
+import MaterialIcon from "material-icons-react";
 
-// FIXME: Hr currently has a tiny width for some reason
-const divider = styled.hr`
-
+const Divider = styled.hr`
+  width: 100%;
 `;
 
 const ProfileScreen = styled.main`
   background: #30323a;
+  color: #fff;
   display: flex;
   flex-direction: column;
   margin: 0 35px 0 35px;
@@ -28,33 +28,86 @@ const Greeting = styled.span`
   font-size: 36px;
   font-weight: normal;
   text-align: center;
+  margin: 10px 0 0 0;
 `;
 
 const Name = styled.span`
   color: #a9ffc1;
 `;
 
-const PassageCount = styled.p`
+const PassageCount = styled.span`
   font-size: 16px;
   text-align: center;
-  margin: 0 auto 0 auto;
+  margin: 0 auto 10px auto;
 `;
 
-const PassasgeTitle = styled.h1`
+const PassageNum = styled.span`
+  color: #7ec9ff;
+`;
+
+const PassageTitle = styled.span`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   font-size: 30px;
-  font-weight: medium;
-  margin: 0 0 0 0;
+  font-weight: 500;
+  margin: 5px 0 15px 0;
+  padding: 0;
+  text-decoration: none;
+
+  p {
+    margin: 0;
+  }
+`;
+
+const Show = styled.span`
+  color: #aeaeae;
+  font-size: 14px;
+  margin: 0 0 0 35px;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  min-width: 50px;
 `;
 
 const PassageList = styled.div`
   display: flex;
   flex-direction: column;
+  margin: 0 0 25px 0;
+`;
+
+const RightSide = styled.div`
+  color: #9d9d9d;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 16px;
+  margin: 0 0 0 15px;
+  min-width: 110px;
 `;
 
 // Max-width based on a max line length of 15 characters
 const PassageItem = styled.p`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-size: 40px;
+  margin: 0;
+  width: 100%;
+`;
+
+const BookTitle = styled.p`
+  color: #aaafc5;
   font-size: 22px;
   max-width: 170px;
+  margin: 0;
+`;
+
+const BookDesc = styled.p`
+  color: #aeaeae;
+  font-size: 14px;
+  margin: 0 0 12px 0;
 `;
 
 const JSON_stuff = [
@@ -82,7 +135,21 @@ const JSON_stuff = [
 
 const Passage = ({ passage }) => {
   return (
-    <PassageItem>{passage.title}</PassageItem>
+    <PassageItem>
+      <div>
+        <BookTitle>
+          {passage.title}
+        </BookTitle>
+        <BookDesc>
+          {passage.desc}
+        </BookDesc>
+      </div>
+      <RightSide>
+        <p>p.{passage.page}</p>
+        <MaterialIcon icon="bookmark_border" color="#afafaf" size={22} />
+        <MaterialIcon icon="share" color="#afafaf" size={22} />
+      </RightSide>
+    </PassageItem>
   );
 };
 
@@ -103,16 +170,18 @@ const Profile = ({ username, imgSource, passageCount }) => {
 
       <Greeting>Hey <Name>{username}</Name>,</Greeting>
       <PassageCount>
-        You've read {passageCount} passages
+        You've read <PassageNum>{passageCount}</PassageNum> passages
       </PassageCount>
 
-      <hr />
-      <PassasgeTitle>Passages<BookmarkBorder></BookmarkBorder></PassasgeTitle>
-      <PassageList>{passage_list}</PassageList>
-      <hr />
-
-      
-
+      <Divider />
+      <PassageTitle>
+        <p>Passages</p>
+        <Show>
+          Show
+          <MaterialIcon icon="bookmarks" color="#a7a7a7" size={20} />
+        </Show>
+      </PassageTitle>
+      <PassageList>{passage_list}</PassageList>      
     </ProfileScreen>
   );
 }
